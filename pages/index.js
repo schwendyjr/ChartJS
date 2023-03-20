cda36c5f0dc67edfadbfe604fb7941a731265f9d
schwendyjr <113371678+schwendyjr@users.noreply.github.com>
2023-03-20 14:41:01 -0700

    new files 1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import BarChart from '@/Components/BarChart'
+import BarChart from '@/components/BarChart'
 
 
 export default function Home() {
