fbc5ce4a5147be355a2618712ba45aa5bdebc9f6
schwendyjr <113371678+schwendyjr@users.noreply.github.com>
2023-03-20 14:38:25 -0700

    fixed Components typo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import BarChart from '@/components/BarChart'
+import BarChart from '@/Components/BarChart'
 
 
 export default function Home() {
